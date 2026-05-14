f82b233d12b2ebb2ccd3d8ef297c7cd7389288cc
Václav Zadina <vaclav.zadina@tul.cz>
2026-05-14 07:55:26 +0200

    Fix Spring Boot main class configuration

---
+++ backend/src/main/java/com/currency/currency_analyzer/CurrencyAnalyzerApplication.java
@@ -0,0 +1,13 @@
+package com.currency.currency_analyzer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CurrencyAnalyzerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CurrencyAnalyzerApplication.class, args);
+	}
+
+}
